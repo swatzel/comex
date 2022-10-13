5b9732e3940ed9135a715d25f5b2ca1f9db4163d
swatzel <swatzel@gmail.com>
2022-10-13 14:10:57 -0300

    enviando tarefa essencial - MainListagemCategoria

---
+++ src/br/com/comex/main/MainListagemCategoria.java
@@ -0,0 +1,35 @@
+package br.com.comex.main;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.com.comex.csv.ConnectionFactory;
+
+public class MainListagemCategoria {
+
+	public static void main(String[] args) throws SQLException {
+
+		ConnectionFactory connectionfactory = new ConnectionFactory();
+		try (Connection connection = connectionfactory.recuperarConexao()){
+		
+		try (PreparedStatement stm = connection.prepareStatement("SELECT ID, NOME, STATUS FROM COMEX.CATEGORIA"))
+		{
+		stm.execute();
+		try (ResultSet rst = stm.getResultSet())
+		{
+		while (rst.next()) {
+			Integer id = rst.getInt("id");
+			String nome = rst.getString("nome");
+			String status = rst.getString("status");
+			
+			System.out.println(id + " | " + nome + " | " + status);
+		
+			
+	      }
+		}
+	  }
+	}
+  }
+}
