f7b33460c01abacd43da80de2602375302f117b0
swatzel <swatzel@gmail.com>
2022-10-17 09:54:21 -0300

    enviando CRUD Produto - Listagem de Produto

---
+++ src/br/com/comex/main/MainListagemProduto.java
@@ -0,0 +1,42 @@
+package br.com.comex.main;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.com.comex.csv.ConnectionFactory;
+
+public class MainListagemProduto {
+
+	public static void main(String[] args) throws SQLException {
+		ConnectionFactory connectionfactory = new ConnectionFactory();
+		try (Connection connection = connectionfactory.recuperarConexao()){
+		
+		try (PreparedStatement stm = connection.prepareStatement("select * from COMEX.PRODUTO"))
+		{
+		stm.execute();
+		try (ResultSet rst = stm.getResultSet())
+		{
+		while (rst.next()) {
+			Integer id = rst.getInt("id");
+			String nome = rst.getString("nome");
+			String descricao = rst.getString("descricao");
+			Integer precoUnitario = rst.getInt("preco_unitario");
+			Integer quantidadeEstoque = rst.getInt("quantidade_estoque");
+			Integer categoriaId = rst.getInt("categoria_id");
+			String tipo = rst.getString("tipo");
+			
+			System.out.println("ID: " + id + " | " + nome + " | Descrição: " +  descricao +
+					  " | R$ " + precoUnitario + " | Qtd Estoque: " + quantidadeEstoque +
+					  " | Categoria_ID: " + categoriaId + " | Tributação: " + tipo);
+		
+			
+	      }
+		}
+	  }
+	}
+  }
+}
+
+
