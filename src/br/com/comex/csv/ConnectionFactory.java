d2b0181be2fcd45bdb2183fa17f0cdadb0f06f2b
swatzel <swatzel@gmail.com>
2022-10-17 09:23:44 -0300

    enviando opcional - pool de conexoes

--- src/br/com/comex/csv/ConnectionFactory.java
+++ src/br/com/comex/csv/ConnectionFactory.java
@@ -1,14 +1,30 @@
 package br.com.comex.csv;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
+	
+	public DataSource dataSource;
+	
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:xe");
+		comboPooledDataSource.setUser("system");
+		comboPooledDataSource.setPassword("stephany");
+		
+		comboPooledDataSource.setMaxPoolSize(15);
+		
+		this.dataSource = comboPooledDataSource;
+	}
 
 	public Connection recuperarConexao() throws SQLException {
-		return DriverManager.getConnection
-				("jdbc:oracle:thin:@localhost:1521:xe", "system", "stephany");				
+		return dataSource.getConnection();				
+
 			
 	}
 }
