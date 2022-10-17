b6f5c484b8ee30e73124fcbc16dfcbc8cbd82fe8
swatzel <swatzel@gmail.com>
2022-10-17 09:55:36 -0300

    enviando CRUD Produto - Remocao de Produto

---
+++ src/br/com/comex/main/MainRemocaoProduto.java
@@ -0,0 +1,32 @@
+package br.com.comex.main;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.com.comex.csv.ConnectionFactory;
+
+public class MainRemocaoProduto {
+
+	public static void main(String[] args) throws SQLException {
+		
+		ConnectionFactory connectionfactory = new ConnectionFactory();
+		try (Connection connection = connectionfactory.recuperarConexao()){
+		
+		try (PreparedStatement stm = connection.prepareStatement
+				("delete from COMEX.PRODUTO where id = ?")){
+		
+		stm.setLong(1, 189);;
+		
+		stm.execute();
+		
+		
+		Integer linhasRemovidas = stm.getUpdateCount();
+		
+		System.out.println("Quantidade de linhas removidas: " + linhasRemovidas);
+		
+		
+      }
+    }
+  }
+}
