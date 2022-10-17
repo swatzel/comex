92d4d3077768be1208b59eda93aa86ce1cc9d446
swatzel <swatzel@gmail.com>
2022-10-17 16:39:29 -0300

    implementa Remocao CategoriaDao

---
+++ src/br/com/comex/dao/main/MainRemoveCategoriaDao.java
@@ -0,0 +1,24 @@
+package br.com.comex.dao.main;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import br.com.comex.csv.ConnectionFactory;
+import br.com.comex.dao.CategoriaDao;
+
+public class MainRemoveCategoriaDao {
+
+	public static void main(String[] args)throws SQLException {
+		Connection conexao = new ConnectionFactory().recuperarConexao();
+		
+		CategoriaDao categoriaDao = new CategoriaDao(conexao);
+			
+		categoriaDao.remove(309L);
+			
+		conexao.close();
+		
+		System.out.println("Categoria removida com sucesso");
+
+	}
+
+}
