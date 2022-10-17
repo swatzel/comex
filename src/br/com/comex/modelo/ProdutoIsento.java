8af9ec48ec779be7b3d3d66d477b3e7474b01cb9
swatzel <swatzel@gmail.com>
2022-10-17 09:46:03 -0300

    enviando adaptacao ProdutoIsento

--- src/br/com/comex/modelo/ProdutoIsento.java
+++ src/br/com/comex/modelo/ProdutoIsento.java
@@ -3,15 +3,15 @@ package br.com.comex.modelo;
 public class ProdutoIsento extends Produto {
 	
 	
-		public ProdutoIsento(String nome, String descricao, double precoUnitario, int quantidadeEstoque,
-			Categoria categoria) {
-		super(nome, descricao, precoUnitario, quantidadeEstoque, categoria);
+		public ProdutoIsento( String nome, String descricao, double precoUnitario, int quantidadeEstoque,
+			Categoria categoria, ProdutoTributavel produtotributavel) {
+		super(nome, descricao, precoUnitario, quantidadeEstoque, categoria, produtotributavel);
 				
 	}
 
 		public double getCalculaImposto(){
 		return 0;
 		
-		}
+	}
 	
 	}
