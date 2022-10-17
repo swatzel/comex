949ad7b2ca745b1783cee444e96deaa9530b79a0
swatzel <swatzel@gmail.com>
2022-10-17 16:40:14 -0300

    implementa insercao CategoriaDao

---
+++ src/br/com/comex/dao/main/MainInsercaoCategoriaDao.java
@@ -0,0 +1,27 @@
+package br.com.comex.dao.main;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import br.com.comex.csv.ConnectionFactory;
+import br.com.comex.dao.CategoriaDao;
+import br.com.comex.modelo.Categoria;
+import br.com.comex.modelo.StatusCategoria;
+
+public class MainInsercaoCategoriaDao {
+
+	public static void main(String[] args) throws SQLException {
+		Categoria novaCategoria = new Categoria ("MOVEIS", StatusCategoria.ATIVA);
+		
+		Connection conexao = new ConnectionFactory().recuperarConexao();
+		
+		CategoriaDao categoriaDao = new CategoriaDao(conexao);
+		categoriaDao.insere(novaCategoria);
+		
+		conexao.close();
+		
+		System.out.println("Categoria criada com sucesso");
+
+	}
+
+}
