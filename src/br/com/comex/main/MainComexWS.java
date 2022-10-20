8fc9b14aabd55d56e1f65175852c33bfbf256479
swatzel <swatzel@gmail.com>
2022-10-20 16:53:40 -0300

    enviando tarefa essencial - MainComexWS

---
+++ src/br/com/comex/main/MainComexWS.java
@@ -0,0 +1,21 @@
+package br.com.comex.main;
+
+import javax.xml.ws.Endpoint;
+
+import br.com.comex.ws.ComexWS;
+
+public class MainComexWS {
+
+	public static void main(String[] args) {
+
+		ComexWS service = new ComexWS();
+		String url = "http://localhost:8080/ComexWS";
+		
+		System.err.println("Serviço rodando " + url);
+		
+		Endpoint.publish(url, service);
+		
+
+	}
+
+}
