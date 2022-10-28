c9dca09d74a2fa296cd99d002065b82cc2877e98
swatzel <swatzel@gmail.com>
2022-10-28 14:00:09 -0300

    enviando tarefa opcional - Volumes no container no Oracle

--- oracle.sh
+++ oracle.sh
@@ -1,1 +1,1 @@
-docker run -d -p 1522:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
+docker run -d --name Oracle -p 1522:1521 -e ORACLE_PASSWORD=admin -v oracle-volume:/opt/oracle/oradata gvenzl/oracle-xe
